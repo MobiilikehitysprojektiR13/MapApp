c26e335daaf35c25928c37b6eb562a6fa5e4ff74
asrii00 <t2rias00@students.oamk.fi>
2024-04-07 10:36:33 +0300

    Update MarkersScreen.kt
    Combined Din's remove marker code with the UI code i wrote before.

--- app/src/main/java/com/example/mapapp/screens/markers/MarkersScreen.kt
+++ app/src/main/java/com/example/mapapp/screens/markers/MarkersScreen.kt
@@ -1,16 +1,24 @@
 package com.example.mapapp.screens.markers
 
 import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,11 +26,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mapapp.datastore.StoreMarkers
+import com.example.mapapp.models.TextMarker
 import com.example.mapapp.screens.markers.viewmodels.MarkersViewModel
 import org.osmdroid.util.GeoPoint
 
@@ -39,25 +49,61 @@ fun MarkersScreen() {
     LaunchedEffect(markers) {
         markersStore.saveMarkers(markers)
     }
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        markers.forEach {
-            item {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                ) {
-                    Row(
-                        modifier = Modifier.padding(8.dp),
-                        verticalAlignment = Alignment.CenterVertically
+    Column ( modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(
+            text = "Map Markers",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            markers.forEach {
+                item {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
                     ) {
-                        Text(modifier = Modifier.weight(1f), text = it.text)
-                        IconButton(onClick = {
-                            Log.d("DeleteMarker", "Deleting marker with index: ${it.index}")
-                            markersViewModel.deleteMarker(it.index)
-                        }
+                        Row(
+                            modifier = Modifier.padding(8.dp),
+                            verticalAlignment = Alignment.CenterVertically
                         ) {
-                            Icon(imageVector = Icons.Default.Delete, contentDescription = "")
+                            Icon(
+                                imageVector = Icons.Default.LocationOn,
+                                contentDescription = "Map Marker",
+                                modifier = Modifier.size(40.dp)
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Column(
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                Text(
+                                    text = it.text,
+                                    style = MaterialTheme.typography.titleMedium,
+                                    modifier = Modifier.padding(bottom = 4.dp)
+
+                                )
+                                Text(
+                                    text = geoPointToLatitudeText(it.geoPoint),
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    color = Color.Gray
+                                )
+                                Text(
+                                    text = geoPointToLongitudeText(it.geoPoint),
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    color = Color.Gray
+                                )
+                            }
+                            Spacer(modifier = Modifier.width(16.dp))
+                            IconButton(onClick = {
+                                Log.d("DeleteMarker", "Deleting marker with index: ${it.index}")
+                                markersViewModel.deleteMarker(it.index)
+                            }
+                            ) {
+                                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+                            }
                         }
                     }
                 }
@@ -67,9 +113,6 @@ fun MarkersScreen() {
 
 }
 
-fun geoPointToText(geoPoint: GeoPoint): String {
-    return "Lat: ${geoPoint.latitude}, Long: ${geoPoint.longitude}"
-}
 
 fun geoPointToLatitudeText(geoPoint: GeoPoint): String {
     return "Lat: ${geoPoint.latitude}"
